686105f32911f9a49124c9f4292240985f156ab4
Anson Zeall <ubuntu@ip-10-212-115-78.ec2.internal>
2013-07-05 17:44:05 +0000

    changed the Hello World from Index.html

--- bitstarter/web.js
+++ bitstarter/web.js
@@ -4,7 +4,7 @@ var fs = require ('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Startup Engineering Course');
+  response.send(fs.readFileSync('./index.html').toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
