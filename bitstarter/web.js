bbfb5873c10cdc7c4510dbdaddb8e88303a1242b
Anson Zeall <ubuntu@ip-10-212-115-78.ec2.internal>
2013-07-05 17:27:02 +0000

    changed to Hello World 2

--- bitstarter/web.js
+++ bitstarter/web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var fs = require ('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
