f82ba1422d5daf8c85610ee761b6f288d9542d45
Anson Zeall <ubuntu@ip-10-212-115-78.ec2.internal>
2013-07-05 17:34:47 +0000

    trying something

--- bitstarter/web.js
+++ bitstarter/web.js
@@ -4,7 +4,7 @@ var fs = require ('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readSyncFile('./index.html').toString('utf-8'));
+  response.send('Startup Engineering Course');
 });
 
 var port = process.env.PORT || 5000;
